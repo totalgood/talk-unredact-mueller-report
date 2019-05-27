06220b4e16f9556e430ab9a9c42cf00b44a2aece
hobs <hobs+github@totalgood.com>
2019-05-26 18:17:46 -0700

    models already here?

--- examples/muellerbot/bernie.py
+++ examples/muellerbot/bernie.py
@@ -16,6 +16,7 @@ MODEL_FILENAME = 'models/bern.iter999.h5'
 TEXT_FILENAME = 'bernie_corpus.txt'
 #TEXT_FILENAME = '../wiki_corpus.txt'
 
+
 def main():
     text = train_bernie.read_text_from_file(TEXT_FILENAME)
     char_indices, indices_char = make_char_lookup_table(text)
@@ -43,6 +44,31 @@ def load_model(char_indices):
 
 
 def ask_bernie(model, question, char_indices, indices_char):
+    sentence = re.sub(r'\W+', ' ', question).lower()
+
+    sys.stdout.write("Output: ")
+    c = '.'
+    for c in sentence:
+        predict(model, c, char_indices, indices_char, temperature=0.0, batch_size=1)
+
+    characters = []
+    for i in range(512):
+        temp = 0.8 if c == ' ' else 0.2
+        c = predict(model, c, char_indices, indices_char, temperature=0.1, batch_size=1)
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        characters.append(c)
+    sys.stdout.write('\n')
+    model.reset_states()
+
+    generated = ''.join(characters)
+    # Strip off last word and capitalize sentences
+    generated = ' '.join(generated.split()[:-1])
+    generated = '. '.join([s.strip().capitalize() for s in generated.split('.')]) + '.'
+    return generated
+
+
+def ask_mueller(model, question, char_indices, indices_char):
     sentence = re.sub(r'\W+', ' ', question).lower()
 
     sys.stdout.write("Output: ")
